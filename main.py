de64c8a0be6d9eb6c691e7a77a9cb2fc0f894f99
Sanahul <110530900+Sanahul@users.noreply.github.com>
2026-02-15 07:11:44 +0530

    Updated main.py with new content.

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
-# Updated main.py to disable the default help command and set a custom help command
-
+import os
 import discord
 from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
 
 intents = discord.Intents.default()
+intents.message_content = True
 
-# Create a bot instance with help_command set to None
 bot = commands.Bot(command_prefix='!', intents=intents, help_command=None)
 
-@bot.command(name='commands')
-async def commands(ctx):
-    await ctx.send("Available commands:") # Add your custom command responses here
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has connected to Discord!')
+    await bot.change_presence(activity=discord.Game(name='!ping'))
 
-# Other bot events and commands can go here
+@bot.command(name='ping')
+async def ping(ctx):
+    await ctx.send(f'Pong! {round(bot.latency * 1000)}ms')
 
+@bot.command(name='hello')
+async def hello(ctx):
+    await ctx.send(f'Hello {ctx.author.name}!')
 
-# Start the bot
-bot.run('YOUR_TOKEN')
+bot.run(TOKEN)
