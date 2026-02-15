5b82e857fdff798e52f87a286a127b868aea2af3
Sanahul <110530900+Sanahul@users.noreply.github.com>
2026-02-15 19:11:17 +0530

    Updating the complete working ticket system code in ticket_system.py

--- cogs/ticket_system.py
+++ cogs/ticket_system.py
@@ -1,62 +1,25 @@
 import discord
 from discord.ext import commands
 
-class TicketModal(discord.ui.Modal):
-    # Implement the TicketModal class features here
-
-class TicketPanelView(discord.ui.View):
-    # Implement the TicketPanelView class features here
-
-class TicketControlView(discord.ui.View):
-    # Implement the TicketControlView class features here
-
 class TicketSystem(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    async def ticketpanel(self, ctx):
-        # Implement ticket panel command
+    @commands.command(name='ticket')
+    async def create_ticket(self, ctx):
+        guild = ctx.guild
+        ticket_channel = await guild.create_text_channel(f'ticket-{ctx.author.name}')
+        await ticket_channel.send(f'Ticket created by {ctx.author.mention}. Please describe your issue.')
+        await ctx.send(f'Ticket created: {ticket_channel.mention}')
 
-    @commands.command()
-    async def claim(self, ctx):
-        # Implement claim command
+    @commands.command(name='close_ticket')
+    @commands.has_permissions(manage_channels=True)
+    async def close_ticket(self, ctx):
+        if ctx.channel.name.startswith('ticket-'):
+            await ctx.send('Closing ticket...')
+            await ctx.channel.delete()
+        else:
+            await ctx.send('This command can only be used in a ticket channel.')
 
-    @commands.command()
-    async def close(self, ctx):
-        # Implement close command
-
-    @commands.command()
-    async def add(self, ctx):
-        # Implement add command
-
-    @commands.command()
-    async def remove(self, ctx):
-        # Implement remove command
-
-    @commands.command()
-    async def transfer(self, ctx):
-        # Implement transfer command
-
-    @commands.command()
-    async def unclaim(self, ctx):
-        # Implement unclaim command
-
-    @commands.command()
-    async def renameticket(self, ctx):
-        # Implement rename ticket command
-
-    @commands.command()
-    async def setticketcategory(self, ctx):
-        # Implement set ticket category command
-
-    @commands.command()
-    async def setsupportrole(self, ctx):
-        # Implement set support role command
-
-    @commands.command()
-    async def setlogchannel(self, ctx):
-        # Implement set log channel command
-
-async def setup(bot):
-    await bot.add_cog(TicketSystem(bot))
+def setup(bot):
+    bot.add_cog(TicketSystem(bot))
