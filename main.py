afc5d6dde5fc76da0556f10ed76292173bf48176
Sanahul <110530900+Sanahul@users.noreply.github.com>
2026-02-15 08:00:22 +0530

    Removed the !mminfo command.

--- main.py
+++ main.py
@@ -1,20 +1,2 @@
-import discord
-from discord.ext import commands
-
-class MatchmakingInfo(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-
-    @commands.command(name='mminfo')
-    async def mminfo(self, ctx):
-        embed = discord.Embed(
-            title='Matchmaking Information',
-            color=discord.Color.blue()
-        )
-        embed.add_field(name='Status', value='Currently matchmaking is underway.', inline=False)
-        embed.add_field(name='Estimated Time', value='2-5 minutes', inline=False)
-        embed.add_field(name='Players in Queue', value='42', inline=False)
-        await ctx.send(embed=embed)
-
-def setup(bot):
-    bot.add_cog(MatchmakingInfo(bot))
+# main.py
+# The !mminfo command has been removed. If you want to re-add matchmaking info, add a Cog or command here.
