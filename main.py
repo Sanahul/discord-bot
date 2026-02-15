d8dfffc57f04f898a12ab85bcba1047d0c0df230
Sanahul <110530900+Sanahul@users.noreply.github.com>
2026-02-15 08:39:43 +0530

    Removed all commands and retained only setup and on_ready event.

--- main.py
+++ main.py
@@ -1,23 +1,11 @@
 import discord
-from discord.ext import commands
 
 intents = discord.Intents.default()
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = discord.Client(intents=intents)
 
-@bot.command()
-@commands.has_permissions(manage_messages=True)
-async def purge(ctx, amount: int):
-    if amount <= 0:
-        await ctx.send("Please provide a number greater than 0.")
-        return
-    deleted = await ctx.channel.purge(limit=amount)
-    await ctx.send(f'Deleted {len(deleted)} messages.', delete_after=5)
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}')
 
-@purge.error
-async def purge_error(ctx, error):
-    if isinstance(error, commands.MissingPermissions):
-        await ctx.send("You do not have permission to use this command.")
-    elif isinstance(error, commands.BadArgument):
-        await ctx.send("Please provide a valid number of messages to purge.")
-
-bot.run('your_token_here')
+# Token should be kept secret
+bot.run('YOUR_TOKEN_HERE')
