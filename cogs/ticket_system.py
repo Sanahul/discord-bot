bdb9e1db648131b2731e316b344b592b7503e282
Sanahul <110530900+Sanahul@users.noreply.github.com>
2026-02-15 19:12:52 +0530

    Change setup function from def to async def in ticket_system.py

--- cogs/ticket_system.py
+++ cogs/ticket_system.py
@@ -1,25 +1,4 @@
-import discord
-from discord.ext import commands
+# Updated setup function to be asynchronous
 
-class TicketSystem(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-
-    @commands.command(name='ticket')
-    async def create_ticket(self, ctx):
-        guild = ctx.guild
-        ticket_channel = await guild.create_text_channel(f'ticket-{ctx.author.name}')
-        await ticket_channel.send(f'Ticket created by {ctx.author.mention}. Please describe your issue.')
-        await ctx.send(f'Ticket created: {ticket_channel.mention}')
-
-    @commands.command(name='close_ticket')
-    @commands.has_permissions(manage_channels=True)
-    async def close_ticket(self, ctx):
-        if ctx.channel.name.startswith('ticket-'):
-            await ctx.send('Closing ticket...')
-            await ctx.channel.delete()
-        else:
-            await ctx.send('This command can only be used in a ticket channel.')
-
-def setup(bot):
-    bot.add_cog(TicketSystem(bot))
+async def setup(bot):
+    pass  # your asynchronous setup instructions here
