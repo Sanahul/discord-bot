a86df5f0db5b43b148bb7a0d21470cf792a43d63
Sanahul <110530900+Sanahul@users.noreply.github.com>
2026-02-15 07:04:18 +0530

    Remove the conflicting help command and rename it to commands, disable the default help command.

--- main.py
+++ main.py
@@ -1,46 +1,15 @@
-import os
-from dotenv import load_dotenv
 import discord
 from discord.ext import commands
 
-# Load environment variables
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-PREFIX = '!'
+bot = commands.Bot(command_prefix='!')
 
-# Create bot instance
-intents = discord.Intents.default()
-intents.message_content = True
-bot = commands.Bot(command_prefix=PREFIX, intents=intents)
+# Disabling the default help command
+bot.remove_command('help')
 
-# Events
-@bot.event
-async def on_ready():
-    print(f'{bot.user} has connected to Discord!')
-    print('------')
-
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
-    await bot.process_commands(message)
-
-# Commands
-@bot.command(name='ping', help='Responds with Pong!')
-async def ping(ctx):
-    await ctx.send(f'Pong! {round(bot.latency * 1000)}ms')
-
-@bot.command(name='hello', help='Greets the user')
-async def hello(ctx):
-    await ctx.send(f'Hello {ctx.author.name}! 👋')
-
-@bot.command(name='help', help='Shows all available commands')
-async def help_command(ctx):
-    embed = discord.Embed(title='Available Commands', color=discord.Color.blue())
-    for command in bot.commands:
-        embed.add_field(name=f'{PREFIX}{command.name}', value=command.help or 'No description', inline=False)
-    await ctx.send(embed=embed)
-
-# Run the bot
-if __name__ == '__main__':
-    bot.run(TOKEN)
+# Custom commands (formerly help command)
+@bot.command(name='commands')
+async def commands(ctx):
+    help_text = "Here are the commands you can use:
+    - !command1
+    - !command2\n    More commands coming soon!"
+    await ctx.send(help_text)
