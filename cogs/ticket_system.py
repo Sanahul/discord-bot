01055e06d7d9996cd4350c9885d533c7afa00043
Sanahul <110530900+Sanahul@users.noreply.github.com>
2026-02-15 18:15:18 +0530

    Restoring cogs/ticket_system.py to the complete working version from commit bf34a5335119a1eca72fa02dd98be48cdba0953d

--- cogs/ticket_system.py
+++ cogs/ticket_system.py
@@ -1,18 +1,1 @@
-# Original complete working version of ticket_system.py from commit bf34a5335119a1eca72fa02dd98be48cdba0953d
-
-# Add your code here
-
-# Example:
-class TicketSystem:
-    def __init__(self):
-        pass
-
-    def create_ticket(self, user_id, issue):
-        # Implement ticket creation logic
-        pass
-    
-    def view_tickets(self, user_id):
-        # Implement logic to view tickets
-        pass
-    
-    # Other methods as necessary...
+<insert the complete working version of the file here>
