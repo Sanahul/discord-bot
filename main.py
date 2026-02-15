661b3aa65919a4826798216542efab76125856c3
Sanahul <110530900+Sanahul@users.noreply.github.com>
2026-02-15 07:54:52 +0530

    Replace main.py contents with a working Discord bot code including ping and hello commands

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
-# Import necessary libraries
-... # Your code here (update with actual implementation)
+import os
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
 
-# Using $ prefix instead of !
-@bot.command()
-async def ticket(ctx):
-    await ctx.send("Ticket system activated!")
-    # More ticket panel system code
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents, help_command=None)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has connected to Discord!')
+    await bot.change_presence(activity=discord.Game(name='!ping'))
+
+@bot.command(name='ping')
+async def ping(ctx):
+    await ctx.send(f'Pong! {round(bot.latency * 1000)}ms')
+
+@bot.command(name='hello')
+async def hello(ctx):
+    await ctx.send(f'Hello {ctx.author.name}!')
+
+bot.run(TOKEN)
